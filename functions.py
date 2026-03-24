69d9734498c5b726132b0c51e1b08b7fb61db352
Vkhodykin <victor.khodykin@gmail.com>
2026-03-24 21:33:21 +0300

    Добавил проверку типа переменной в calculate_sum

--- functions.py
+++ functions.py
@@ -2,6 +2,10 @@
 
 def calculate_sum(n: int | float) -> int | float:
 
+    if not isinstance(n, int) or not isinstance(n, float):
+
+        raise TypeError("n must be int or float")
+
     summa = 0
 
     for i in range (1, n + 1):

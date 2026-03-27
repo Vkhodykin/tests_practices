ea9d14803feddb6d73a846f7eaad65bd06124960
Vkhodykin <victor.khodykin@gmail.com>
2026-03-28 00:40:56 +0300

    Добавил тесты функции count_words в test_functions.py

--- test_functions.py
+++ test_functions.py
@@ -1,5 +1,6 @@
 import pytest
 from functions import calculate_sum
+from functions import count_words
 
 
 def test_calculate_sum_str():
@@ -33,3 +34,29 @@ def test_calculate_sum_negative():
 def test_calculate_sum_positive():
     with pytest.raises(TypeError):
         calculate_sum(10)
+
+
+def test_count_words_single_spaces():
+    assert count_words("HELLO, MR. RAK!") == 3
+
+def test_count_words_void():
+    with pytest.raises(TypeError):
+        count_words(None)
+
+def test_count_words_only_spaces():
+    assert count_words("     ") == 0
+
+def test_count_words_spaces_front_behind():
+    assert count_words("  HELLO, MR. RAK!  ") == 3
+
+def test_count_words_multiple_spaces():
+    assert count_words("HELLO,   MR.   RAK!") == 3
+
+def test_single_word():
+    assert count_words("HELLO") == 1
+
+def test_count_words_single_letters():
+    assert count_words("d s g e") == 4
+
+def test_count_words_hyphenated_words():
+    assert count_words("python-cool") == 1
